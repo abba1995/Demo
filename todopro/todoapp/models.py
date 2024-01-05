662a4a67ab72200f5b6141c21e9d5e73a5422e7f
abba1995 <150160872+abba1995@users.noreply.github.com>
2024-01-05 09:29:06 +0530

    Delete todopro directory

--- todopro/todoapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Task(models.Model):
-    name=models.CharField(max_length=250)
-    priority=models.IntegerField(max_length=10)
-    date=models.DateField()
-    def __str__(self):
-        return self.name
